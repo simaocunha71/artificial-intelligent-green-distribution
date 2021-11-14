7164639bcfddf1d38ad94d44e0424b869518e168
brazafonso <61985783+brazafonso@users.noreply.github.com>
2021-11-14 17:00:02 +0000

    ideia inicial das structs

---
+++ dados.pl
@@ -0,0 +1,34 @@
+\*
+estafetas(
+      identificacao,
+      zona,
+      meio_transporte,
+      classificacao_media,
+      classificacoes_totais,
+      lista das entregas
+)
+*/
+
+
+
+/*
+meio_transporte(
+    tipo,
+    velocidade
+)
+*/
+
+
+
+/*
+pedido(
+    cliente,
+    prazo,
+    zona,
+    peso,
+    preco,
+    data,
+    estado
+)
+
+*/
