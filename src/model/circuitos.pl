4278043bea12d60c196c53ebafdae8252a80d12c
simaocunha71-miei <a93262@alunos.uminho.pt>
2021-12-13 11:35:02 +0000

    grafos das zonas

--- src/model/circuitos.pl
+++ src/model/circuitos.pl
@@ -1,11 +1,70 @@
 % ------------------------ Circuitos ------------------------------ %
 
 % Ruilhe
+aresta("Rua 1","Rua 8" ,15).
+aresta("Rua 1","Rua 18",14).
+
+aresta("Rua 8","Rua 10",5).
+aresta("Rua 8","Rua 3" ,14).
+aresta("Rua 8","Rua 5" ,10).
+aresta("Rua 8","Rua 6" ,14).
+
+aresta("Rua 3","Rua 4" ,1).
+aresta("Rua 3","Rua 6" ,8).
+
+aresta("Rua 4","Rua 19",18).
+
+aresta("Rua 5","Rua 10",6).
+aresta("Rua 5","Rua 6" ,14).
+
+aresta("Rua 18","Rua 19",15).
+aresta("Rua 18","Rua 6", 7).
+
 
 % Lomar
+aresta("Rua 0","Rua 11", 19).
+aresta("Rua 0","Rua 10", 14).
+aresta("Rua 0", "Rua 3", 1).
+aresta("Rua 0","Rua 16", 16).
+
+aresta("Rua 1", "Rua 9", 16).
+aresta("Rua 1", "Rua 10", 4).
+aresta("Rua 1", "Rua 11", 11).
+
+aresta("Rua 3","Rua 16", 6).
+
+aresta("Rua 9", "Rua 10", 7).
+aresta("Rua 9", "Rua 11", 10).
+aresta("Rua 9", "Rua 3", 15).
+
 
 % Cabreiros
+aresta("Rua 1", "Rua 4", 3).
+aresta("Rua 1", "Rua 10",4).
+aresta("Rua 4", "Rua 10",2).
 
 % Ferreiros
+aresta("Rua 10", "Rua 11", 7).
+aresta("Rua 10", "Rua 18", 6).
+aresta("Rua 11", "Rua 18", 2).
 
 % Semelhe
+aresta("Rua 1", "Rua 0", 2).
+aresta("Rua 1", "Rua 7", 2).
+aresta("Rua 1", "Rua 15", 12).
+aresta("Rua 0", "Rua 15", 14).
+aresta("Rua 0", "Rua 3", 18).
+aresta("Rua 0", "Rua 19", 19).
+aresta("Rua 0", "Rua 8", 16).
+aresta("Rua 0", "Rua 12", 7).
+aresta("Rua 15", "Rua 16", 10).
+aresta("Rua 12", "Rua 13", 10).
+aresta("Rua 12", "Rua 16", 19).
+aresta("Rua 12", "Rua 3", 12).
+aresta("Rua 3", "Rua 7", 6).
+aresta("Rua 3", "Rua 19", 17).
+aresta("Rua 3", "Rua 11", 6).
+aresta("Rua 11", "Rua 8", 17).
+aresta("Rua 19", "Rua 16", 16).
+aresta("Rua 7", "Rua 18", 14).
+aresta("Rua 13", "Rua 18", 10).
